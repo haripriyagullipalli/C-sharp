899a9188fd6c635cf639ba92c9d8f61f3b07fbc2
Haripriya <183710614+haripriyagullipalli@users.noreply.github.com>
2025-06-08 16:51:13 +0530

    adds weather simulator

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -1,2 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Diagnostics;
+using System.Runtime.Intrinsics.Arm;
+
+class WeatherSimulator
+{
+
+    public static void Main()
+    {
+
+        System.Console.WriteLine("enter numbers of days for weather simulator");
+        int days = int.Parse(Console.ReadLine());
+
+        int[] temparatures = new int[days];
+        string[] conditions = { "snowy", "cloudy", "rainy", "sunny" };
+        string[] weatherConditions = new string[days];
+
+        for (int i = 0; i < days; i++)
+        {
+            Random random = new Random();
+            temparatures[i] = random.Next(0, 40);
+            weatherConditions[i] = conditions[random.Next(conditions.Length)];
+            System.Console.WriteLine("temperature " + temparatures[i] + "conditon i " + weatherConditions[i]);
+        }
+
+
+        System.Console.WriteLine("the average is " + AverageTemperature(temparatures, days));
+    }
+
+    static double AverageTemperature(int[] temperatures, int days)
+    {
+        int sum = temperatures.Aggregate(0, (initial, current) => initial + current);
+        return sum / days;
+    }
+}
+
